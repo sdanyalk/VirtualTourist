d59a25fb35eaaf5af4465487c234b6e412966310
s.danyal.k@gmail.com <Laynad@123>
2019-05-12 14:18:24 -0700

    added Photo Album View Controller

---
+++ VirtualTourist/Controller/PhotoAlbumViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by SDK on 5/12/19.
+//  Copyright © 2019 SDK. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
